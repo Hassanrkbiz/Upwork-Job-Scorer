b9b9d415fd63f14d69e2897b0d36d83ac7d69d9f
Hassan <Razahassan554@gmail.com>
2022-03-27 00:40:09 +0500

    first commit

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,90 @@
-import { printLine } from './modules/print';
+import { Arrive } from './modules/arrive';
+import { helper } from './modules/helper';
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
 
-printLine("Using the 'printLine' function from the Print Module");
+console.log('content script running');
+
+document.arrive(
+  '[data-test="job-tile-list"] > section.up-card-section',
+  function (elem) {
+    console.log('elem created');
+    MainFunc([elem]);
+  }
+);
+
+function MainFunc(jobCards) {
+  if (jobCards.length > 0) {
+    jobCards.forEach((card, index) => {
+      var exist = card.querySelectorAll(".upworkjobscoreext");
+      if (exist.length < 1) {
+        var TotalJobScore = 0;
+        var count = 0;
+        var jobType = card.querySelectorAll('strong[data-test="job-type"]');
+        if (jobType.length > 0) {
+          var JobTypeScore = helper.getJobTypeScore(jobType[0].innerText);
+          // console.log(JobTypeScore);
+          TotalJobScore += parseFloat(JobTypeScore);
+          count += 1;
+        }
+        var clientPaymentStatus = card.querySelectorAll(
+          'strong[class="text-muted"]'
+        );
+        if (clientPaymentStatus.length > 0) {
+          var PaymentStatusScore = helper.getClientPaymentStatus(
+            clientPaymentStatus[0].innerText
+          );
+          // console.log(PaymentStatusScore);
+          TotalJobScore += parseFloat(PaymentStatusScore);
+          count += 1;
+        }
+        var clientPaid = card.querySelectorAll(
+          '[data-test="client-spendings"] > strong'
+        );
+        if (clientPaid.length > 0) {
+          var ClientPaidScore = helper.getClientPaid(clientPaid[0].innerText);
+          // console.log(ClientPaidScore);
+          TotalJobScore += parseFloat(ClientPaidScore);
+          count += 1;
+        }
+        var clientRating = card.querySelectorAll(
+          ".up-rating-background > span"
+        );
+        if (clientRating.length > 0) {
+          var ClientRatingScore = helper.getClientRating(
+            clientRating[0].innerText
+              .replace("Rating is ", "")
+              .replace(" out of 5.", "")
+          );
+          // console.log(ClientRatingScore);
+          TotalJobScore += parseFloat(ClientRatingScore);
+          count += 1;
+        }
+        var JobPostingTime = card.querySelectorAll('[data-test="posted-on"]');
+        if (JobPostingTime.length > 0) {
+          var JobPostingScore = helper.getJobPostingTime(
+            JobPostingTime[0].innerText
+          );
+          // console.log(JobPostingScore);
+          TotalJobScore += parseFloat(JobPostingScore);
+          count += 1;
+        }
+        // console.log(TotalJobScore);
+        TotalJobScore = (TotalJobScore / count).toFixed(1);
+        if (TotalJobScore > 6.9 && TotalJobScore < 10.1) {
+          createJobBadge("greenJobSE", card, TotalJobScore);
+        } else if (TotalJobScore > 2.9 && TotalJobScore < 7) {
+          createJobBadge("yellowJobSE", card, TotalJobScore);
+        } else if (TotalJobScore > -0.1 && TotalJobScore < 3) {
+          createJobBadge("redJobSE", card, TotalJobScore);
+        }
+      }
+    });
+  }
+}
+
+function createJobBadge(classN, card, score) {
+  var div = document.createElement("div");
+  div.className = "upworkjobscoreext";
+  div.innerHTML = `<h2 class="${classN}">${score}</h2>`;
+  card.appendChild(div);
+}
---
+++ src/pages/Content/modules/helper.js
@@ -0,0 +1,88 @@
+export const helper = {
+  getJobTypeScore: (job) => {
+    if (job == "Fixed-price") {
+      return 7;
+    } else {
+      return 10;
+    }
+  },
+  getClientPaymentStatus: (job) => {
+    if (job == "Payment verified") {
+      return 10;
+    } else {
+      return 0;
+    }
+  },
+  getClientRating: (rating) => {
+    if (rating) {
+      return (parseFloat(rating) * 2).toFixed(1);
+    } else {
+      return 0;
+    }
+  },
+  getClientPaid: (paid) => {
+    paid = paid.replace("+", "").replace("$", "");
+    if (paid.toString().includes("k")) {
+      paid.replace("k", "");
+      paid = parseInt(paid);
+      paid = paid * 1000;
+    }
+    if (paid.toString().includes("m")) {
+      paid.replace("m", "");
+      paid = parseInt(paid);
+      paid = paid * 1000000;
+    }
+    paid = parseInt(paid);
+    if (paid == 0) {
+      return 0;
+    } else if (paid < 100 && paid > 0) {
+      return 1;
+    } else if (paid > 99 && paid < 500) {
+      return 2;
+    } else if (paid > 499 && paid < 1000) {
+      return 3;
+    } else if (paid > 999 && paid < 5000) {
+      return 4;
+    } else if (paid > 4999 && paid < 10000) {
+      return 5;
+    } else if (paid > 9999 && paid < 50000) {
+      return 6;
+    } else if (paid > 49999 && paid < 100000) {
+      return 7;
+    } else if (paid > 99999 && paid < 500000) {
+      return 8;
+    } else if (paid > 499999 && paid < 1000000) {
+      return 9;
+    } else if (paid > 999999) {
+      return 10;
+    }
+  },
+  getJobPostingTime: (time) => {
+    var timeNum = time.match(/([0-9]+)/g);
+    if (timeNum !== null) {
+      timeNum = parseInt(timeNum[0]);
+      if (time.toString().includes("hour")) {
+        timeNum = timeNum * 60 * 60;
+      } else if (time.toString().includes("minute")) {
+        timeNum = timeNum * 60;
+      } else if (time.toString().includes("day")) {
+        timeNum = timeNum * 60 * 60 * 24;
+      } else if (time.toString().includes("month")) {
+        timeNum = timeNum * 60 * 60 * 24 * 30;
+      }
+      if (timeNum < 3600) {
+        return 10;
+      } else if (timeNum > 3599 && timeNum < 10800) {
+        return 7;
+      } else if (timeNum > 10799 && timeNum < 18000) {
+        return 5;
+      } else if (timeNum > 17999 && timeNum < 86400) {
+        return 3;
+      } else if (timeNum > 86399) {
+        return 0;
+      }
+    } else {
+      return 0;
+    }
+  },
+};
